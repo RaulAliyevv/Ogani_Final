a1dc530718980087d89d57cc31b009e8a563f7bf
RaulAliyevv <raulza@code.edu.az>
2025-03-17 23:23:58 +0400

    Add project files.

---
+++ Ogani.Core/Entities/Base/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace Ogani.Core.Entities.Base;
+
+public abstract class BaseEntity
+{
+    public int Id { get; set; }
+}
+
---
+++ Ogani.Core/Entities/Product.cs
@@ -0,0 +1,33 @@
+﻿using Ogani.Core.Entities.Base;
+
+namespace Ogani.Core.Entities;
+
+public class Product : BaseEntity
+{
+    public string Name { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public decimal Price { get; set; }
+    public string IsMainPicture { get; set; } = null!;
+    public List<ProductCategory> ProductCategories { get; set; } = [];
+    public List<ProductImage>? ProductImages { get; set; } = [];
+}
+
+public class Category : BaseEntity
+{
+    public string Name { get; set; } = null!;
+    public string ImageUrl { get; set; } = null!;
+}
+public class ProductCategory : BaseEntity
+{
+    public int CategoryId { get; set; }
+    public Category? Category { get; set; }
+    public int ProductId { get; set; }
+    public Product? Product { get; set; }
+}
+
+public  class ProductImage : BaseEntity
+{
+    public  string ImageUrl { get; set; } = null!;
+    public int ProductId { get; set; }
+    public Product? Product { get; set; }
+}
